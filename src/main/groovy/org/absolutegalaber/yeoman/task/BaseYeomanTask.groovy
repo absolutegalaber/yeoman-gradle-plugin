8a686b2c0dbe3dec84ac351d44f4c7d17fa5e2b7
Josip Mihelko <josip.mihelko@googlemail.com>
2015-04-19 12:31:00 +0200

    #4 Fixed Version info log on windows. New Version 0.3.0

--- src/main/groovy/org/absolutegalaber/yeoman/task/BaseYeomanTask.groovy
+++ src/main/groovy/org/absolutegalaber/yeoman/task/BaseYeomanTask.groovy
@@ -12,26 +12,37 @@ abstract class BaseYeomanTask extends DefaultTask {
 
     public void logToolVersion(String tool) {
         logger.quiet("###### Version of ${tool}:")
+        List<String> theCommand = commandToExecute([tool, '--version'])
         project.exec {
             workingDir = project.file(project.extensions.yeoman.yeomanDir)
-            commandLine = [tool, '--version']
+            commandLine = theCommand
         }
     }
 
     public void executeCommand(List<String> command) {
         logger.info("Executing: ${command} in ${project.extensions.yeoman.yeomanDir}")
-        List<String> theCommand = new ArrayList<>();
-        if (isWindows()) {
-            logger.quiet("##### Running on Windows")
-            theCommand.add("cmd")
-            theCommand.add("/c")
-        }
-        theCommand.addAll(command)
+        List<String> theCommand = commandToExecute(command)
         logger.quiet("Command to be executed: {}", theCommand)
         project.exec {
             workingDir = project.file(project.extensions.yeoman.yeomanDir)
             commandLine = theCommand
         }
+    }
+
+    /**
+     * Adds 'cmd /c' in fornt of a command line if being executed on windows.
+     * @param command The original command line to be eecuted
+     * @return A command line that is executable on windows.
+     */
+    private List<String> commandToExecute(List<String> command) {
+        List<String> toReturn = new ArrayList<>()
+        if (isWindows()) {
+            logger.quiet("##### Running on Windows ==> using cmd /c {command}")
+            toReturn.add('cmd')
+            toReturn.add('/c')
+        }
+        toReturn.addAll(command)
+        toReturn
     }
 
     private boolean isWindows() {
